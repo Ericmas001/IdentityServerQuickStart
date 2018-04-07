040d35f268ae64cd2e19bb10cfec3edfcd58bfe4
Éric Massé <Ericmas001@gmail.com>
2018-04-07 09:56:24 -0400

    Adding IdentityServer4

--- IdentityServerQuickStart/Startup.cs
+++ IdentityServerQuickStart/Startup.cs
@@ -15,6 +15,8 @@ namespace IdentityServerQuickStart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +26,7 @@ namespace IdentityServerQuickStart
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseIdentityServer();
         }
     }
 }
