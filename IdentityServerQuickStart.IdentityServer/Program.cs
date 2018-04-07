1b8a1602fc753b6bcf42b44099b3584e433e03a3
Éric Massé <Ericmas001@gmail.com>
2018-04-07 10:11:16 -0400

    Simple Identity Server

--- IdentityServerQuickStart.IdentityServer/Program.cs
+++ IdentityServerQuickStart.IdentityServer/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace IdentityServerQuickStart
+namespace IdentityServerQuickStart.IdentityServer
 {
     public class Program
     {
--- IdentityServerQuickStart.IdentityServer/Startup.cs
+++ IdentityServerQuickStart.IdentityServer/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace IdentityServerQuickStart
+namespace IdentityServerQuickStart.IdentityServer
 {
     public class Startup
     {
@@ -15,8 +15,11 @@ namespace IdentityServerQuickStart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure identity server with in-memory stores, keys, clients and resources
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential();
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
