9fc25c00c989ee710504113465fe9614df100d9a
Éric Massé <Ericmas001@gmail.com>
2018-04-07 16:20:08 -0400

    Initial Commit BetterIdentityServer

---
+++ IdentityServerQuickStart.BetterIdentityServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IdentityServerQuickStart.BetterIdentityServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
